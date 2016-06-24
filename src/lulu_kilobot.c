72a53a8208d47861b924a1ee806b1124841867ce
Andrei Florea <andrei91ro@gmail.com>
2016-06-25 00:36:42 +0300

    working fsm

---
+++ src/lulu_kilobot.c
@@ -0,0 +1,336 @@
+/**
+ * @file lulu_kilobot.c
+ * @brief Lulu P colony simulator extension that allows controlling a Kilobot robot by using a P colony based controller
+ * In this file we implement all methods that allow a complete Kilobot application to be built.
+ * @author Andrei G. Florea
+ * @author Catalin Buiu
+ * @date 2016-03-06
+ */
+
+#include "lulu_kilobot.h"
+#include "instance.h"
+#include "debug_print.h"
+
+#ifdef SIMULATOR
+    char* motionNames[] = {"stop", "straight", "left", "right"};
+    char* colorNames[] = {"off", "red", "green", "blue", "white"};
+#endif
+
+//get *mydata to the coresponding USERDATA structure of the running robot
+REGISTER_USERDATA(USERDATA)
+
+void set_motion(motion_t dir_new)
+{
+	//if the same direction is requested again
+	if (dir_new == mydata->current_motion_state)
+		//skip request in order to avoid an unneeded engine spinup
+		return;
+	switch (dir_new)
+	{
+		case MOTION_STOP:
+			set_motors(0, 0);
+			break;
+
+		case MOTION_STRAIGHT:
+			spinup_motors();
+			set_motors(kilo_straight_left, kilo_straight_right);
+			break;
+
+		case MOTION_LEFT:
+			spinup_motors();
+			set_motors(kilo_turn_left, 0);
+			break;
+
+		case MOTION_RIGHT:
+			spinup_motors();
+			set_motors(0, kilo_turn_right);
+			break;
+	}
+	mydata->current_motion_state = dir_new;
+}
+
+void handle_default() {
+    printd("called default handler");
+}
+
+void handle_neighbor_close() {
+    uint8_t rand_value, options_count = 4;
+    printd("handle neighbor close");
+    //set_color(RGB(3, 0, 0));
+    //mydata->current_led_color = COLOR_RED;
+
+#ifndef KILOBOT
+    //use rand() from stdlib.h
+    //rand_value in [0; options_count-1] interval
+    rand_value = rand() % options_count;
+#else
+    //use rand_soft from kilolib.h
+    //rand_value in [0; options_count-1] interval
+    rand_value = rand_soft() % options_count;
+#endif
+
+    switch (rand_value) {
+        case 0: case 1: {
+            mydata->current_led_color = COLOR_GREEN;
+            set_motion(MOTION_STRAIGHT);
+            break;
+        }
+        case 2: {
+            mydata->current_led_color = COLOR_RED;
+            set_motion(MOTION_LEFT);
+            break;
+        }
+        case 3: {
+            mydata->current_led_color = COLOR_BLUE;
+            set_motion(MOTION_RIGHT);
+            break;
+        }
+    }
+
+    set_color(colorValues[mydata->current_led_color]);
+}
+
+void handle_all_neighbors_distant() {
+    printd("handle neighbor distant");
+    set_color(RGB(3, 3, 3));
+    mydata->current_led_color = COLOR_WHITE;
+    set_motion(MOTION_STOP);
+}
+
+void forget_neighbors() {
+    for (uint8_t i = 0; i < MAX_NEIGHBORS; i++)
+        // if the deadline for forgeting this neighbor has passed
+        if (kilo_ticks >= mydata->neighbors[i].timexp_forget && mydata->neighbors[i].timexp_forget > 0) {
+            //decrease the number of neighbors
+            mydata->nr_neighbors = (mydata->nr_neighbors > 0)? mydata->nr_neighbors - 1: 0;
+            //re-initialize this neighbor
+            mydata->neighbors[i].uid = NO_ID;
+            mydata->neighbors[i].symbolic_id = NO_ID;
+            mydata->neighbors[i].distance = 0;
+            mydata->neighbors[i].distance_prev = 0;
+            mydata->neighbors[i].timexp_forget = 0;
+        }
+}
+
+void process_message() {
+    uint8_t i;
+
+
+    uint8_t distance = estimate_distance(&RB_front().dist);
+    uint8_t *data = RB_front().msg.data;
+    uint16_t id = data[INDEX_MSG_OWNER_UID_LOW] | data[INDEX_MSG_OWNER_UID_HIGH] << 8;
+
+    //search for the robot uid in the current neighbor list
+    for (i = 0; i < MAX_NEIGHBORS; i++)
+        if (mydata->neighbors[i].uid == id)
+            break;
+
+    //if the sender of this message was not found in the current list of neighbors
+    if (i == MAX_NEIGHBORS)
+        //check for an empty slot
+        for (i = 0; i < MAX_NEIGHBORS; i++)
+            if (mydata->neighbors[i].uid == NO_ID) {
+                //we will remember a new neighbor
+                mydata->nr_neighbors++;
+                break;
+            }
+
+    //if there was no empty slot
+    if (i == MAX_NEIGHBORS) {
+        printw("kilo_uid: %d - no slot for KB%d", kilo_uid, id);
+        return;
+    }
+
+    //if we reach this step then i is a valid slot
+    mydata->neighbors[i].uid = id;
+    mydata->neighbors[i].symbolic_id = id - smallest_robot_uid;
+    //if there is no previous recording of the distance to this neighbor
+    if (mydata->neighbors[i].distance == mydata->neighbors[i].distance_prev &&
+           mydata->neighbors[i].distance == 0)
+        //save current distance in both fields
+        mydata->neighbors[i].distance = mydata->neighbors[i].distance_prev = distance;
+    else {
+        //save previous distance
+        mydata->neighbors[i].distance_prev = mydata->neighbors[i].distance;
+        //save current distance
+        mydata->neighbors[i].distance = distance;
+    }
+
+    //set the moment in the future when the robot will forget about this neighbor
+    mydata->neighbors[i].timexp_forget = kilo_ticks + FORGET_NEIGHBOR_INTERVAL;
+}
+
+void procInputModule() {
+    uint8_t i; //used for iterating through the neighbor list
+    bool dist_big = TRUE;
+
+    for (i = 0; i < MAX_NEIGHBORS; i++)
+        if (mydata->neighbors[i].distance < PARAM_DISTANCE_THRESHOLD && mydata->neighbors[i].uid != NO_ID) {
+            dist_big = FALSE;
+            break;
+        }
+    //if (dist_big || mydata->nr_neighbors == 0)
+    if (dist_big)
+        //replaceObjInMultisetObj(&mydata->pcol.agents[AGENT_MSG_DISTANCE].obj, OBJECT_ID_D_ALL, OBJECT_ID_B_ALL);
+        mydata->current_event = EVENT_ALL_NEIGHBORS_DISTANT;
+    else
+        //replaceObjInMultisetObj(&mydata->pcol.agents[AGENT_MSG_DISTANCE].obj, OBJECT_ID_D_ALL, OBJECT_ID_S_ALL);
+        mydata->current_event = EVENT_NEIGHBOR_CLOSE;
+}
+
+void procOutputModule() {
+    #ifdef USING_AGENT_MOTION
+        for (uint8_t obj_id = 0; obj_id < mydata->pcol.agents[AGENT_MOTION].obj.size; obj_id++)
+            switch (mydata->pcol.agents[AGENT_MOTION].obj.items[obj_id]) {
+                case OBJECT_ID_M_0: set_motion(MOTION_STOP); break;
+                case OBJECT_ID_M_S: set_motion(MOTION_STRAIGHT); break;
+                case OBJECT_ID_M_L: set_motion(MOTION_LEFT); break;
+                case OBJECT_ID_M_R: set_motion(MOTION_RIGHT);break;
+            }
+    #endif
+
+    #ifdef USING_AGENT_LED_RGB
+        //for (uint8_t obj_id = 0; obj_id < mydata->pcol.agents[AGENT_LED_RGB].obj.size; obj_id++)
+            //switch (mydata->pcol.agents[AGENT_LED_RGB].obj.items[obj_id]) {
+                //case OBJECT_ID_C_0: mydata->current_led_color = COLOR_OFF; break;
+                //case OBJECT_ID_C_R: mydata->current_led_color = COLOR_RED; break;
+                //case OBJECT_ID_C_G: mydata->current_led_color = COLOR_GREEN; break;
+                //case OBJECT_ID_C_B: mydata->current_led_color = COLOR_BLUE; break;
+                //case OBJECT_ID_C_W: mydata->current_led_color = COLOR_WHITE; break;
+            //}
+        //set_color(colorValues[mydata->current_led_color]);
+    #endif
+}
+
+message_t* message_tx() {
+    return &mydata->msg_tx;
+}
+
+void message_rx(message_t* msg, distance_measurement_t* dist) {
+    //add one message at the tail of the receive ring buffer
+    Received_message_t *newMsg = &RB_back();
+    newMsg->msg = *msg;
+    newMsg->dist = *dist;
+    //now RB_back() will point to an empty slot in the buffer
+    RB_pushback();
+}
+
+void setup_message() {
+    mydata->msg_tx.type = NORMAL;
+    mydata->msg_tx.data[INDEX_MSG_OWNER_UID_LOW] = kilo_uid & 0xFF; //low byte
+    mydata->msg_tx.data[INDEX_MSG_OWNER_UID_HIGH] = kilo_uid >> 8; //high byte
+    mydata->msg_tx.crc = message_crc(&mydata->msg_tx);
+}
+
+void loop() {
+#ifdef PCOL_SIM
+    printi("\nLOOP for robot %d\n-------------------------\n", kilo_uid);
+#endif
+    //if the previous step was the last one
+    //if (mydata->sim_result == SIM_STEP_RESULT_NO_MORE_EXECUTABLES) {
+        ////mark the end of the simulation and exit
+        //set_color(RGB(0, 2, 0));
+        //set_motion(MOTION_STOP);
+        //printi("kilo_uid %d: NO_MORE_EXEC", kilo_uid);
+        //return;
+    //} else
+    //// if the previous step resulted in an error
+    //if (mydata->sim_result == SIM_STEP_RESULT_ERROR) {
+        //set_color(RGB(2, 0, 0));
+        //set_motion(MOTION_STOP);
+        ////we don't print any error message here because we wouldn't be able to read the LULU error because this continuous print
+        //return;
+    //}
+
+    //remove old neighbors that haven't sent any recent message
+    forget_neighbors();
+
+    //process the entire received message buffer
+    while (!RB_empty()) {
+        process_message();
+        RB_popfront();
+    }
+
+#ifdef USING_ID_SECURITY
+    if (kilo_uid == STRANGER_UID) {
+        set_color(RGB(3, 0, 3)); //magenta
+        return;
+    }
+#endif
+    //transform sensor input into symbolic objects
+    procInputModule();
+    //mydata->sim_result = pcolony_runSimulationStep(&mydata->pcol);
+    //transform symbolic objects into effector commands
+    //procOutputModule();
+
+    handlers[mydata->current_event]();
+#ifdef KILOBOT
+    //prevent the real kilobot from moving chaotically because of a too high sim step execution speed
+    delay(SLEEP_MS_BETWEEN_SIMSTEPS);
+#endif
+}
+
+void setup() {
+    //initialize the mydata structure
+    //mydata->current_led_color = COLOR_OFF;
+    //mydata->current_motion_state = MOTION_STOP;
+    //mydata->sim_result = SIM_STEP_RESULT_FINISHED;
+    //mydata->light = mydata->light_prev = 0;
+    mydata->nr_neighbors = 0;
+    mydata->neighbor_index = 0;
+    mydata->current_event = EVENT_ALL_NEIGHBORS_DISTANT;
+    //initialize message for transmission
+    setup_message();
+
+    //initialize Pcolony
+    lulu_init(&mydata->pcol);
+
+    #ifdef NEEDING_WILDCARD_EXPANSION
+        expand_pcolony(&mydata->pcol, kilo_uid);
+    #endif
+
+    //init neighbors
+    for (uint8_t i = 0; i < MAX_NEIGHBORS; i++)
+        mydata->neighbors[i] = (Neighbor_t) {
+            .uid = NO_ID,
+            .symbolic_id = NO_ID,
+            .distance = 0,
+            .distance_prev = 0,
+            .timexp_forget = 0};
+
+    //initialize message receive buffer
+    RB_init();
+}
+
+#ifdef SIMULATOR
+/* provide a text string for the simulator status bar about this bot */
+char *cb_botinfo(void)
+{
+    char *p = botinfo_buffer;
+    p += sprintf (p, "ID: %d, MOTION: %s, COLOR: %s \n", kilo_uid, motionNames[mydata->current_motion_state], colorNames[mydata->current_led_color]);
+
+    p += sprintf (p, "\n nr_neighbors = %d ", mydata->nr_neighbors);
+    for (uint8_t i = 0; i < MAX_NEIGHBORS; i++)
+        if (mydata->neighbors[i].uid != NO_ID)
+            p += sprintf (p, "n[%d]={%d, %d}, ", i, mydata->neighbors[i].uid, mydata->neighbors[i].distance);
+    return botinfo_buffer;
+}
+#endif
+
+int main() {
+    kilo_init();
+    #ifdef DEBUG
+        // initialize serial module (only on Kilobot)
+	    debug_init();
+    #endif
+    //callbacks for Kilombo (resolve to empty functions if building for Kilobot)
+    SET_CALLBACK(botinfo, cb_botinfo);
+
+    //register kilobot callbacks
+    kilo_message_rx = message_rx;
+    kilo_message_tx = message_tx;
+
+    kilo_start(setup, loop);
+
+    return 0;
+}
---
+++ src/lulu_kilobot.h
@@ -0,0 +1,274 @@
+// vim:filetype=c
+/**
+ * @file lulu_kilobot.h
+ * @brief Lulu P colony simulator extension that allows controlling a Kilobot robot by using a P colony based controller
+ * In this header we declare the structure and functions of a basic Kilobot controller that is based on P colonies and implements a bidirectional symbolic object - numeric value conversion.
+ * This application uses the Kilombo simulator and also the kilolib library for interfacing with both a simulated and real kilobot.
+ * @author Andrei G. Florea
+ * @author Catalin Buiu
+ * @date 2016-03-06
+ */
+
+#ifndef LULU_KILOBOT_H
+#define LULU_KILOBOT_H
+
+//on the Kilobot, debug functions such as printf have to be specifically included with debug.h
+#if defined(DEBUG_PRINT) && defined(KILOBOT)
+    //enables UART serial module
+    #define DEBUG
+    #include "debug.h"
+#endif
+
+#include <kilombo.h> //for access to sensors, motors (includes kilolib.h)
+#include "lulu.h"
+
+#define MAX_NEIGHBORS 20
+#define RB_SIZE 8 //ring buffer size
+
+//we define a NO_ID value for Neighbor_t.uid, because 0 can be used as a regular uid
+#define NO_ID UINT16_MAX
+
+#ifdef USING_ID_SECURITY
+    //the stranger robot doesn't move (is moved by the user), emits messages as normal robots and is magenta coloured
+    //this robot is used in ID based security demos
+    #define STRANGER_UID 2
+    //#define STRANGER_UID 62
+#endif
+
+#define FORGET_NEIGHBOR_INTERVAL 32 * 2 //forget neighbors if the last msg received was 32 * X seconds ago (1 second = 32 kiloticks)
+#define SLEEP_MS_BETWEEN_SIMSTEPS 50 //the number of miliseconds to wait before executing the next Lulu simulation step
+
+/**
+ * @brief Define the states used for a robot
+ */
+typedef enum {
+    STATE_STOP_LED_OFF,
+    STATE_LEFT_RED,
+    STATE_RIGHT_BLUE,
+    STATE_STRAIGHT_GREEN,
+    STATE_STOP_WHITE
+} state_t;
+
+/**
+ * @brief Define the events that can be perceived
+ */
+typedef enum {
+    EVENT_NEIGHBOR_CLOSE,
+    EVENT_ALL_NEIGHBORS_DISTANT
+} event_t;
+
+/**
+ * @brief Default finite state machine event handler - does not change anything
+ */
+void handle_default();
+
+void handle_neighbor_close();
+
+void handle_all_neighbors_distant();
+
+void (*handlers[2])(void) = {handle_neighbor_close, handle_all_neighbors_distant};
+
+
+/**
+ * @brief Define motion types
+ */
+typedef enum {
+    MOTION_STOP,
+    MOTION_STRAIGHT,
+    MOTION_LEFT,
+    MOTION_RIGHT
+} motion_t;
+
+typedef enum {
+    COLOR_OFF,
+    COLOR_RED,
+    COLOR_GREEN,
+    COLOR_BLUE,
+    COLOR_WHITE
+    //TODO add all basic colors used in lulu_kilobot.py
+} led_color_t;
+
+uint8_t colorValues[] = {RGB(0, 0, 0), RGB(3, 0, 0), RGB(0, 3, 0), RGB(0, 0, 3), RGB(3, 3, 3)};
+
+enum {
+    INDEX_MSG_OWNER_UID_LOW,
+    INDEX_MSG_OWNER_UID_HIGH,
+};
+
+/**
+ * @brief Structure that holds information about a recent neighbor robot
+ */
+typedef struct _Neighbor {
+    uint16_t uid;
+    uint16_t symbolic_id;
+    uint8_t distance, distance_prev;
+    uint32_t timexp_forget;
+} Neighbor_t;
+
+/**
+ * @brief Structure that holds information about a received message
+ * This information cannot be processed imediately after receiving because estimating the ditance is a time consuming task that cannot be executed inside an intrerupt.
+ */
+typedef struct _received_message {
+    message_t msg;
+    distance_measurement_t dist;
+} Received_message_t;
+
+typedef struct {
+    Pcolony_t pcol;
+
+    //uint8_t light,
+            //light_prev;
+    motion_t current_motion_state;
+    led_color_t current_led_color;
+    //sim_step_result_t sim_result;
+
+    Neighbor_t neighbors[MAX_NEIGHBORS];
+    uint8_t nr_neighbors,
+            neighbor_index;
+
+    //transmitted message
+    message_t msg_tx;
+
+    //receive messages ring buffer (taken from kilombo/examples/gradient2)
+    uint8_t RXHead, RXTail;
+    Received_message_t RXBuffer[RB_SIZE];
+
+    //the currently perceived event
+    event_t current_event;
+
+} USERDATA;
+
+/*-------------------- RING BUFFER DEFINITION ---------------------------*/
+//this definition was taken from kilombo/examples/gradient2/gradient.h
+//available at: https://github.com/JIC-CSB/kilombo/blob/master/examples/gradient2/gradient.h
+
+// Ring buffer operations. Taken from kilolib's ringbuffer.h
+// but adapted for use with mydata->
+
+// Ring buffer operations indexed with head, tail
+// These waste one entry in the buffer, but are interrupt safe:
+//   * head is changed only in popfront
+//   * tail is changed only in pushback
+//   * RB_popfront() is to be called AFTER the data in RB_front() has been used
+//   * head and tail indices are uint8_t, which can be updated atomically
+//     - still, the updates need to be atomic, especially in RB_popfront()
+
+#define RB_init() {	\
+    mydata->RXHead = 0; \
+    mydata->RXTail = 0;\
+}
+
+#define RB_empty() (mydata->RXHead == mydata->RXTail)
+
+#define RB_full()  ((mydata->RXHead+1)%RB_SIZE == mydata->RXTail)
+
+#define RB_front() mydata->RXBuffer[mydata->RXHead]
+
+#define RB_back() mydata->RXBuffer[mydata->RXTail]
+
+#define RB_popfront() mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;
+
+
+#define RB_pushback() {\
+    mydata->RXTail = (mydata->RXTail+1)%RB_SIZE;\
+    if (RB_empty())\
+      { mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;	\
+	/*printf("Full.\n"); */}			\
+  }
+
+/*-------------------- END RING BUFFER DEFINITION -----------------------*/
+
+#ifdef SIMULATOR
+    /* provide a text string for the simulator status bar about this bot */
+    static char botinfo_buffer[10000];
+    extern char* motionNames[];
+    extern char* colorNames[];
+#endif
+
+//TODO params need tunning
+#define PARAM_LIGHT_THRESHOLD 20
+#define PARAM_DISTANCE_THRESHOLD 55
+
+//end event handlers
+
+/**
+ * @brief Remove old neighbors from the neighbor array.
+ * Old neighbors are considered neighbors from which a message was received more than FORGET_NEIGHBOR_INTERVAL kilotics ago.
+ *
+ */
+void forget_neighbors();
+
+/**
+ * @brief Process received data and transfer it to the Neighbor_t data structures for further processing and symbolic conversion
+ *
+ */
+void process_message();
+
+/**
+ * @brief Process raw_state info received from sensors and populate the input module agents with significant objects
+ *
+ */
+void procInputModule();
+
+/**
+ * @brief Process the objects present in the output module agents and transform them into commands that are executed by the Kilobot
+ *
+ */
+void procOutputModule();
+
+
+/**
+ * @brief Set the motion direction
+ *
+ * @param dir_new The new motion direction (DIR_{STOP, FORWARD, LEFT, RIGHT})
+ */
+void set_motion(motion_t dir_new);
+
+/**
+ * @brief Function called after receiving a new message
+ *
+ * @param msg Pointer to the new message
+ * @param dist Pointer to the structure that allows to determine the distance from the sender
+ */
+void message_rx(message_t* msg, distance_measurement_t* dist);
+
+/**
+ * @brief Function called when preparing a new message
+ *
+ * @return Pointer to the new message
+ */
+message_t* message_tx();
+
+/**
+ * @brief Function called after the message has been succesfully sent
+ */
+void message_tx_success();
+
+/**
+ * @brief Prepares a new message for sending (with CRC)
+ */
+void setup_message();
+
+/**
+ * @brief Function called in a continuos loop during the runtine of the program
+ */
+void loop();
+
+/**
+ * @brief Function called only once at startup, before entering the continuos loop
+ */
+void setup();
+
+void process_message();
+
+#ifdef SIMULATOR
+/**
+ * @brief Prepares a status bar message for the simulator
+ *
+ * @return The status bar message
+ */
+char *cb_botinfo(void);
+#endif
+
+#endif
